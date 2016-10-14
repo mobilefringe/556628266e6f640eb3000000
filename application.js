c608e2e7ad5be7d85f17960342e7d3b9235781ee
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-14 10:23:51 -0400

    The Bay Centre 1.2  /application.js

--- application.js
+++ application.js
@@ -61,6 +61,32 @@ function mailchimp_subscribe(email){
     })
 }
 
+// function renderHomeHours(container, template, collection){
+//     var item_list = [];
+//     var item_rendered = [];
+//     var template_html = $(template).html();
+//     Mustache.parse(template_html);   // optional, speeds up future uses
+//     item_list.push(collection);    
+//     $.each( item_list , function( key, val ) {
+//         val.day = get_day(val.day_of_week);
+//         var d = new Date();
+//         val.month = get_month(d.getMonth());
+//         val.weekday = addZero(d.getDate());
+//         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
+//             var open_time = new Date (val.open_time);
+//             var close_time = new Date (val.close_time);
+//             val.open_time = convert_hour(open_time);
+//             val.close_time = convert_hour(close_time);    
+//             val.h = val.open_time+ " - " + val.close_time;
+//         } else {
+//             val.h = "Closed";
+//         }
+//         var rendered = Mustache.render(template_html,val);
+//         item_rendered.push(rendered);
+//     });
+//     $(container).html(item_rendered.join(''));
+// }
+
 function renderHomeHours(container, template, collection){
     var item_list = [];
     var item_rendered = [];
@@ -75,8 +101,6 @@ function renderHomeHours(container, template, collection){
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
             var close_time = moment(val.close_time).tz(getPropertyTimeZone());
-            // val.open_time = convert_hour(open_time);
-            // val.close_time = convert_hour(close_time);    
             val.h = open_time.format("h:mma") + " - " + close_time.format("h:mma");
         } else {
             val.h = "Closed";

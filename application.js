02a50cb3324a0aadc23960f83330fbd2286824dd
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-14 10:28:32 -0400

    The Bay Centre 1.2  /application.js

--- application.js
+++ application.js
@@ -94,10 +94,10 @@ function renderHomeHours(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
     item_list.push(collection);    
     $.each( item_list , function( key, val ) {
-        // val.day = get_day(val.day_of_week);
+        val.day = get_day(val.day_of_week);
         var d = moment();
-        // val.month = get_month(d.getMonth());
-        // val.weekday = addZero(d.getDate());
+        val.month = get_month(d.getMonth());
+        val.weekday = addZero(d.getDate());
         var open_time = moment(val.open_time).tz(getPropertyTimeZone());
         var close_time = moment(val.close_time).tz(getPropertyTimeZone());
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){

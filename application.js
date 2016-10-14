ca4396d7631668bf1de6683c3279165073c027c0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-14 10:15:54 -0400

    The Bay Centre 1.2  /application.js

--- application.js
+++ application.js
@@ -70,14 +70,14 @@ function renderHomeHours(container, template, collection){
     $.each( item_list , function( key, val ) {
         val.day = get_day(val.day_of_week);
         var d = moment();
-        val.month = get_month(d.getMonth());
-        val.weekday = addZero(d.getDate());
+        // val.month = get_month(d.getMonth());
+        // val.weekday = addZero(d.getDate());
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = in_my_time_zone(moment(val.open_time), "h:mmA");
             var close_time = in_my_time_zone(moment(val.close_time), "h:mmA");
             // val.open_time = convert_hour(open_time);
             // val.close_time = convert_hour(close_time);    
-            val.h = open_time + " - " + close_time;
+            val.h = open_time.format("h:mma") + " - " + close_time.format("h:mma");
         } else {
             val.h = "Closed";
         }
